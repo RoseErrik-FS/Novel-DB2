52f5f8c950810ad7bb27d9a9e1b6f7f9f5341094
Errik Rose <errose@student.fullsail.edu>
2024-05-16 23:21:25 -0500

    Updated DB connection to potentially fix prod

--- lib/db.ts
+++ lib/db.ts
@@ -1,5 +1,5 @@
 import { MongoClient } from 'mongodb';
-import mongoose from 'mongoose';
+import mongoose, { ConnectOptions } from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
@@ -7,46 +7,39 @@ if (!MONGODB_URI) {
   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
 }
 
-const globalWithMongo = global as typeof globalThis & {
-  _mongoClientPromise: Promise<MongoClient>;
-};
+// Extend the global object to include custom properties for MongoDB and Mongoose
+declare global {
+  var mongoClient: MongoClient | undefined;
+  var mongooseConnection: Promise<typeof mongoose> | undefined;
+}
 
-let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
-if (process.env.NODE_ENV === 'development') {
-  if (!globalWithMongo._mongoClientPromise) {
-    client = new MongoClient(MONGODB_URI);
-    globalWithMongo._mongoClientPromise = client.connect();
-  }
-  clientPromise = globalWithMongo._mongoClientPromise;
-} else {
-  client = new MongoClient(MONGODB_URI);
+if (!global.mongoClient) {
+  const client = new MongoClient(MONGODB_URI);
   clientPromise = client.connect();
+  global.mongoClient = client;
+} else {
+  clientPromise = global.mongoClient.connect();
 }
 
-export default clientPromise;
+let mongooseConnection: Promise<typeof mongoose>;
 
-// Second database connection for non auth related operations
-const globalWithMongoose = global as typeof globalThis & {
-  mongoose: any;
+if (!global.mongooseConnection) {
+  mongooseConnection = mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  } as ConnectOptions);
+  global.mongooseConnection = mongooseConnection;
+} else {
+  mongooseConnection = global.mongooseConnection;
+}
+
+// Ensure the mongoose connection is established
+const connectToDatabase = async () => {
+  if (mongoose.connection.readyState === 0) await mongooseConnection;
+  return mongoose;
 };
 
-let novelDBClient: typeof mongoose;
-
-async function connectToDatabase() {
-  if (globalWithMongoose.mongoose) {
-    novelDBClient = globalWithMongoose.mongoose;
-  } else {
-    if (typeof MONGODB_URI === 'string') {
-      novelDBClient = await mongoose.connect(MONGODB_URI);
-      globalWithMongoose.mongoose = novelDBClient;
-    } else {
-      throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
-    }
-  }
-
-  return novelDBClient;
-}
-
 export { connectToDatabase };
+export default clientPromise;
